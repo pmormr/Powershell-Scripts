289d7871ebb23e82d96487e5dedf270a14618bb9
Paul Morgan <pmormr@gmail.com>
2020-02-08 16:19:56 -0500

    disable trust tests when no trusts exist

--- audit-active-directory.ps1
+++ audit-active-directory.ps1
@@ -63,13 +63,16 @@ $TestimoConfig.Forest.OptionalFeatures.Tests.LapsAvailable.Enable = $false
 $TestimoConfig.Forest.SiteLinks.Tests.MinimalReplicationFrequency.Parameters.ExpectedValue = 15
 $TestimoConfig.Forest.SiteLinks.Tests.MinimalReplicationFrequency.Parameters.OperationType = "eq"
 $TestimoConfig.Forest.SiteLinks.Tests.UseNotificationsForLinks.Enable = $true
-
 $TestimoConfig.Domain.PasswordComplexity.Tests.LockoutThreshold.Parameters.ExpectedValue = 25
 $TestimoConfig.Domain.PasswordComplexity.Tests.LockoutThreshold.Parameters.OperationType = "eq"
 $TestimoConfig.Domain.PasswordComplexity.Tests.MaxPasswordAge.Parameters.ExpectedValue = 366
 $TestimoConfig.Domain.PasswordComplexity.Tests.MinPasswordLength.Parameters.ExpectedValue = 5
 
+# if there's no trusts, disable trust verification
+$TestTrust = Get-AdTrust -Filter * -Properties *
+if ( $null -eq $TestTrust) {
+    $TestimoConfig.Domain.Trusts.Enable = $false
+}
+
 $TestResults = Invoke-Testimo -Configuration $TestimoConfig -ReportPath "output/current_report.html" -ReturnResults -ShowErrors
 $TestResults | where-object {$_.Status -eq $false } | Format-Table -AutoSize *
-
-
