55dbcb3e5af75e94dd0a0b86ee8f86c914862f28
Paul Morgan <pmormr@gmail.com>
2021-01-26 14:56:15 -0500

    adding ad computer cleanup scripts

---
+++ ad-computer-cleanup.ps1
@@ -0,0 +1,14 @@
+#requires -Version 1 -Modules ActiveDirectory
+
+Get-ADObject -Filter * -SearchBase 'OU=Computer-OU,DC=company,DC=org' | where-object {$_.ObjectClass -like "organizationalUnit"} |
+ForEach-Object -Process {
+    Set-ADObject -ProtectedFromAccidentalDeletion $true -Identity $_ 
+}
+
+
+
+$DaysInactive = 731
+
+$time = (Get-Date).Adddays(-($DaysInactive))
+
+Get-ADComputer -searchbase "CN=Computers,DC=company,DC=org" -Filter {LastLogonTimeStamp -lt $time} -ResultPageSize 2000 -resultSetSize $null -Properties Name, OperatingSystem, SamAccountName, DistinguishedName | remove-adcomputer
